a9159cb5dd90c41a796603c1fe923c0757cf8ad3
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-03 11:27:23 +0530

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -6,16 +6,11 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAvEMr-_GeeAFDfLD3w2oB_s3jPjrPQyZs",
-
-  authDomain: "fragveda.firebaseapp.com",
-
-  projectId: "fragveda",
-
-  storageBucket: "fragveda.appspot.com",
-
-  messagingSenderId: "709002213779",
-
-  appId: "1:709002213779:web:c406148941ed9918cd5066"
+    authDomain: "fragveda.firebaseapp.com",
+    projectId: "fragveda",
+    storageBucket: "fragveda.appspot.com",
+    messagingSenderId: "709002213779",
+    appId: "1:709002213779:web:c406148941ed9918cd5066"
 
 };
 
